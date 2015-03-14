f8ddc677367b536c49aba160e1ef0921cbdab8fa
xmsun <cindyxmsun@gmail.com>
2015-03-13 20:45:24 -0400

    frequency and napoleon text

--- toolbox/image_processing/face_detect.py
+++ toolbox/image_processing/face_detect.py
@@ -1,4 +1,31 @@
-""" Experiment with face detection and image filtering using OpenCV """
-
+import numpy as np 
 import cv2
-import numpy as np
+
+cap =cv2.VideoCapture(0)
+face_cascade= cv2.CascadeClassifier('/Desktop/haarcascade_frontalface_alt.xml')
+kernel = np.ones((90,90),'uint8')
+
+
+while(True):
+    ret, frame = cap.read()
+    faces = face_cascade.detectMultiScale(frame, scaleFactor=1.2, minSize=(20,20))
+    for(x,y,w,h) in faces:
+        frame[y:y+h,x:x+w,:] = cv2.dilate(frame[y:y+h,x:x+w,:], kernel)
+
+        #draw circles at center of frame - eyes?
+        cv2.circle(frame, ((x + w / 2) + 15, (y + h / 2)), 15, (255,255,255), thickness =-1)
+        cv2.circle(frame, ((x + w / 2) - 15, (y + h / 2)), 15, (255,255,255), thickness =-1)
+
+        #face?
+        cv2.ellipse(frame, ((x + w / 2), (y + h / 2 + 10)), (50,50), 180, 180, 360, (255,0,0), thickness = 10) 
+        
+    #Display the resulting frame
+    cv2.imshow('frame',frame)
+    #called periodically to display images and process events
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+#When everything done, release the capture
+
+#releases/closes windwowh when complete
+cap.release()
+cv2.destroyAllWindows()
--- toolbox/word_frequency_analysis/frequency.py
+++ toolbox/word_frequency_analysis/frequency.py
@@ -9,7 +9,28 @@ def get_word_list(file_name):
 		returns a list of the words used in the book as a list.
 		All words are converted to lower case.
 	"""
-	pass
+
+	#open file and read
+	book = open(file_name, 'r')
+	#read file line by line
+	text = book.readlines()
+	#isolate "START OF THIS PROJECT GUTENBERG EBOOK"
+	curr_line = 0
+	while text[curr_line].find('START OF THIS PROJECT GUTENBERG EBOOK') == -1:
+		curr_line += 1
+	#sets text to entire document aside from isolated text
+	text = text[curr_line+1:]
+	#creates a string from the list (text)
+	word_list = ''.join(text)
+	#splits string into list of words
+	word_list = word_list.split()
+
+	#lowercase for every word
+	for word in range(len(word_list)):
+		word_list[word] = word_list[word].lower()
+
+	return word_list
+
 
 def get_top_n_words(word_list, n):
 	""" Takes a list of words as input and returns a list of the n most frequently
@@ -21,4 +42,24 @@ def get_top_n_words(word_list, n):
 		returns: a list of n most frequently occurring words ordered from most
 				 frequently to least frequentlyoccurring
 	"""
-	pass
+
+	#create a dictionary to count number
+	word_counts = dict()
+
+	for word in word_list:
+		#adds new words into list
+		if word not in word_counts:
+			word_counts[word] = 1
+		#word count increases if not already in
+		else:
+			word_counts[word] += 1
+
+	#sorts from highest to lower count
+	ordered_by_frequency = sorted(word_counts, key=word_counts.get, reverse=True)
+
+	#returns top n words with highest frequency
+	return ordered_by_frequency[:n]
+
+
+x = get_word_list('napoleon.txt')
+print get_top_n_words(x, 100)
